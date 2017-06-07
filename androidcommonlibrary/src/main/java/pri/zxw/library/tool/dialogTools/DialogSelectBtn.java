6c39d2d94ae830522d69365b02a494dcb91b5c55
DESKTOP-C8JGK8K\Administrator <jy02537333@gmail.com>
2017-06-07 17:33:42 +0800

    6.7

---
+++ androidcommonlibrary/src/main/java/pri/zxw/library/tool/dialogTools/DialogSelectBtn.java
@@ -0,0 +1,8 @@
+package pri.zxw.library.tool.dialogTools;
+
+/**
+ * Created by Administrator on 2017/6/7.
+ */
+
+public class DialogSelectBtn {
+}
---
+++ app/src/main/java/com/zxw/giftbook/adapter/SendInvitationAdapter.java
@@ -0,0 +1,98 @@
+package com.zxw.giftbook.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.zxw.giftbook.Activity.entitiy.SidekickergroupEntity;
+import com.zxw.giftbook.Activity.menu.SidekickerGroup2Fragment;
+import com.zxw.giftbook.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pri.zxw.library.base.MyBaseAdapter;
+
+/**
+ * 亲友团 ，组名称 适配器
+ *
+ */
+public class SendInvitationAdapter extends MyBaseAdapter {
+    private SidekickerGroup2Fragment mContext;
+    private List<SidekickergroupEntity> comLists;
+    private LayoutInflater inflater = null;
+    @Override
+    public void addDataAll(List infos) {
+        comLists.addAll(infos);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void remove() {
+        comLists.clear();
+    }
+
+    public SendInvitationAdapter(SidekickerGroup2Fragment context) {
+        this.mContext = context;
+        inflater= LayoutInflater.from(context.getContext());
+        comLists=new ArrayList<SidekickergroupEntity>();
+    }
+
+    @Override
+    public int getCount() {
+        if(comLists!=null)
+            return comLists.size();
+        return 0;
+    }
+    @Override
+    public SidekickergroupEntity getItem(int position) {
+        return comLists.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder mHolder;
+        View view = convertView;
+        if (view == null) {
+            view = inflater.inflate(R.layout.item_list_group_type2, null);
+            mHolder = new ViewHolder();
+            mHolder.lay = (LinearLayout) view.findViewById(R.id.item_list_group_type_lay);
+            mHolder.operateImg=(ImageView)view.findViewById(R.id.item_list_group_type_operate_img);
+            mHolder.nameTv = (TextView) view.findViewById(R.id.item_list_group_type_name_tv);
+            mHolder.numTv = (TextView) view.findViewById(R.id.item_list_group_type_num_tv);
+            view.setTag(mHolder);
+        } else {
+            mHolder = (ViewHolder) view.getTag();
+        }
+       final SidekickergroupEntity comInfo =comLists.get(position);
+        mHolder.nameTv.setText(comInfo.getGroupname());
+        mHolder.numTv.setText("("+comInfo.getGroupmembersnum()+")");
+        mHolder.operateImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContext.operate(comInfo.getId(),comInfo.getGroupname());
+            }
+        });
+        return view;
+    }
+
+
+
+
+    class ViewHolder {
+        LinearLayout lay;
+        ImageView operateImg;
+       TextView nameTv;
+        TextView numTv;
+    }
+
+}
